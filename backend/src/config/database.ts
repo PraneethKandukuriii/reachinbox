783931baefed04e3e6c054753e18ee5eedeef9ee
PraneethKandukuriii <praneethkandukuriii@gmail.com>
2026-09-12 13:08:57 +0530

    feat: configure environment and database connection

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,9 +1,9 @@
-import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client.js";
+import { env } from "./env.js";
 
 const adapter =  new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: env.databaseUrl,
 });
 
 const prisma = new PrismaClient({adapter});
