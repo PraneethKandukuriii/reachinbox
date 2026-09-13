8cdf314270172645e189798ed68a5ce9b5b723e5
PraneethKandukuriii <praneethkandukuriii@gmail.com>
2026-09-13 12:08:59 +0530

    refactor: clean up campaign routes

--- backend/src/routes/email.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { createCampaignController } from "../controllers/campaign.controller.js";
-
-const router = Router();
-
-router.post("/:campaignId/emails", createCampaignController);
-
-export default router;
